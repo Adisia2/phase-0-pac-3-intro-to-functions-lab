5dafe506d8ca1cb49832d399694900428cff7783
+John <+>
2022-11-15 12:50:06 +0300

    completed assignment

---
+++ index.js
@@ -0,0 +1,30 @@
+function shout(hello) {
+    return "hello".toUpperCase();
+  }
+
+  function whisper(HELLO){
+    return "HELLO".toLowerCase();
+  }
+  function logShout(hello){
+
+    console.log(hello.toUpperCase());
+  }
+  function logWhisper(HELLO) {
+
+    console.log(HELLO.toLowerCase());
+    
+  }
+  function sayHiToHeadphonedRoommate (hello) {
+    var ansOne = "I can\'t hear you!";
+    var ansTwo = "YES INDEED!";
+    var ansThree = "I would love to!";
+    if (hello.toLowerCase(hello)=== hello) {
+      return ansOne;
+    }
+    else if (hello.toUpperCase(hello)=== hello){
+      return ansTwo;
+    }
+    else if ("Let's have dinner together!" ===  hello) {
+      return ansThree;
+    }
+  }
